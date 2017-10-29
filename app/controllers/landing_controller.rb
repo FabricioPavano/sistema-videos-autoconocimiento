719bb1176daec86edff96ec9afb004c1a92f16a9
Fabricio Pavano <faripavano@gmail.com>
2017-10-29 19:48:59 -0300

    saving work

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,19 +1,8 @@
 class LandingController < ApplicationController
 	def index
-		
-		require './config/environment.rb' # Assuming the script is located in the root of the rails app
 		begin
-		  ActiveRecord::Base.establish_connection # Establishes connection
-		  ActiveRecord::Base.connection # Calls connection object
-		  
-		  @string = "Connected to database"
-
-		  puts "CONNECTED!" if ActiveRecord::Base.connected? 
-		  puts "NOT CONNECTED!" unless ActiveRecord::Base.connected?
+		  @string = "Connected to database" if ActiveRecord::Base.connected? 
 		rescue => e
-			puts e
-		  puts "NOT CONNECTED!"
-
 		  @string = "NOT Connected to database"
 		end
 
