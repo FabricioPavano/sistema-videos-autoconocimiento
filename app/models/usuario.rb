d6770aa189f10978126521937f3c5cc3c6d0f4b5
Fabricio Pavano <faripavano@gmail.com>
2018-02-09 10:51:26 -0300

    guardando mas trabajo

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -9,7 +9,14 @@ class Usuario < ApplicationRecord
   before_create :generate_schedule, :send_welcome_email
 
   def send_welcome_email
-    GnosisMailer.welcome_email(self.email).deliver
+    # GnosisMailer.welcome_email(self.email).deliver
+  end
+
+  def after_confirmation
+    Resque.enqueue_in(1.second, SendTemplateEmailJob, {
+      email: self.email,
+      numero_leccion: 1
+    })
   end
 
   def generate_schedule
@@ -22,10 +29,31 @@ class Usuario < ApplicationRecord
   	schedule.leccion4 = DateTime.now + (interval * 3)
   	schedule.leccion5 = DateTime.now + (interval * 4)
 
-    GnosisMailer.leccion_habilitada_email(self.email, 2).deliver_at( DateTime.now + (interval * 1) )
-    GnosisMailer.leccion_habilitada_email(self.email, 3).deliver_at( DateTime.now + (interval * 2) )
-    GnosisMailer.leccion_habilitada_email(self.email, 4).deliver_at( DateTime.now + (interval * 3) )
+    # GnosisMailer.leccion_habilitada_email(self.email, 2).deliver_at(  )
+    # GnosisMailer.leccion_habilitada_email(self.email, 3).deliver_at( DateTime.now + (interval * 2) )
+    # GnosisMailer.leccion_habilitada_email(self.email, 4).deliver_at( DateTime.now + (interval * 3) )
 
+   
+
+    Resque.enqueue_in(1.days, SendTemplateEmailJob, {
+      email: self.email,
+      numero_leccion: 2
+    })
+
+    Resque.enqueue_in(2.days, SendTemplateEmailJob, {
+      email: self.email,
+      numero_leccion: 3
+    })
+
+    Resque.enqueue_in(3.days, SendTemplateEmailJob, {
+      email: self.email,
+      numero_leccion: 4
+    })
+
+     Resque.enqueue_in(4.days, SendTemplateEmailJob, {
+      email: self.email,
+      numero_leccion: 5
+    })
   end
 
   def set_default_password
